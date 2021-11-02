48e26ef779d74672f624ad5324d505b596415e4c
wan-nyan-wan <distributed.system.love@gmail.com>
2021-11-03 00:27:12 +0900

    Replace existing styles with @emotion/css

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
+import { Global, css } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+
+const GlobalStyle = css`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={GlobalStyle} />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
