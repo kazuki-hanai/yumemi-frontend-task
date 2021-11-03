0cc0a2e32b72f83d94917fcbce504df53a4526fc
wan-nyan-wan <distributed.system.love@gmail.com>
2021-11-03 17:12:57 +0900

    Implement prefecture checkboxes

--- src/components/MainContent/PrefectureCheckBoxes.tsx
+++ src/components/MainContent/PrefectureCheckBoxes.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import { css } from '@emotion/css';
+import usePrefectures from '../../hooks/usePrefectures';
 
-const PrefectureCheckBoxesStyle = css``;
+const PrefectureCheckBoxesStyle = css`
+  width: 100%;
+`;
+const PrefectureUlStyle = css`
+  list-style-type: none;
+  display: flex;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+`;
+const PrefectureListStyle = css`
+  white-space: nowrap;
+`;
 
 const PrefectureCheckBoxes = () => {
+  const endpoint = 'http://localhost:8090';
+  const prefectures = usePrefectures(endpoint);
+
+  const CheckBoxes = [];
+  for (const prefecture of prefectures) {
+    CheckBoxes.push(
+      <li className={PrefectureListStyle}>
+        <label htmlFor={`prefecture-${prefecture.prefCode}`}>
+          {prefecture.prefName}
+          <input
+            type="checkbox"
+            id={`prefecture-${prefecture.prefCode}`}
+            value={prefecture.prefCode}
+          />
+        </label>
+      </li>,
+    );
+  }
   return (
     <div className={PrefectureCheckBoxesStyle}>
-      dummy box
-      <input type="checkbox" value="cb1" />
+      <ul className={PrefectureUlStyle}>{CheckBoxes}</ul>
     </div>
   );
 };
